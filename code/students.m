14348f00c187f668b97f13f5a356cb15a29f1c15
Alden Timme <aldentimme@rescomp-10-107563.Stanford.EDU>
2011-03-09 23:08:07 -0800

    added students file

---
+++ code/students.m
@@ -0,0 +1,6 @@
+StudentInfo(1).fname = 'Alden';
+StudentInfo(1).lname = 'Timme';
+StudentInfo(1).ID = 5408784;
+StudentInfo(2).fname = 'Joseph';
+StudentInfo(2).lname = 'Marrama';
+StudentInfo(2).ID = 5458806;
